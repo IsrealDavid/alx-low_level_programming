6a2b5cb6d4423b3ea05ffe7ca057212c505ce499
Israel David Oluwasegun <isrealdavidsegun@gmail.com>
2022-09-19 18:40:56 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+Void reset_to_98(int *n);
+	Void swap_int(int *a, int *b);
+	Int _strlen(char *s);
+	Void _puts(char c);
+	Int _putchar(char c);
+	Void print_rev(char *s);
+	Void puts2(char *str);
+	Void puts_half(char *str);
+	Void print_array(int *a, int n);
+	Char *_strcpy(char *dest, char *src);
+	Void rev_string(char *s);
+	
