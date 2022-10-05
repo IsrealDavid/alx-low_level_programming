06b7628648ba10a7c6e0dd9d858f0b20f52e0b1f
IsrealDavid <111028717+IsrealDavid@users.noreply.github.com>
2022-10-05 01:17:47 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,41 +1,42 @@
 #include "main.h"
 
 /**
-* str_concat -> string concatinating function
-* @s1: string 1
-* @s2: string 2
-* Return: string 1 + string 2
+* alloc_grid -> allocatingn 2d array
+* @width: width of an array
+* @height: height of an array
+* Return: a pointer to a allocated grid
 */
-char *str_concat(char *s1, char *s2)
+int **alloc_grid(int width, int height)
 {
-	int i = 0, j = 0, l = 0, k = 0;
-	char *s;
+	int i, j, k, l;
+	int **a;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-
-	l = i + j;
-	s = (char *)malloc(l * sizeof(char) + 1);
-	if (s == NULL)
+	if (width <= 0 || height <= 0)
 		return (NULL);
-	j = 0;
-	while (k < l)
+	a = malloc(height * sizeof(int *));
+	if (a == NULL)
 	{
-		if (k < i)
-			s[k] = s1[k];
-		if (k >= i)
-		{
-			s[k] = s2[j];
-			j++;
-		}
-		k++;
+		free(a);
+		return (NULL);
 	}
-	s[k] = '\0';
-	return (s);
+	for (i = 0; i < height; i++)
+	{
+		a[i] = malloc(width * sizeof(int));
+		if (a[i] == NULL)
+		{
+			for (j = i; j >= 0; j--)
+			{
+				free(a[j]);
+			}
+			free(a);
+			return (NULL);
+		}
+	}
+
+	for (k = 0; k < height; k++)
+	{
+		for (l = 0; l < width; l++)
+			a[k][l] = 0;
+	}
+	return (a);
 }
