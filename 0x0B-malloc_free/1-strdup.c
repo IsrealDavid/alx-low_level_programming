ff30cdbaf462121d7783cf3e38e9c089ff705cbe
IsrealDavid <111028717+IsrealDavid@users.noreply.github.com>
2022-10-05 01:08:27 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,3 +15,14 @@ char *_strdup(char *str)
 	while (str[i])
 		i++;
 	s = (char *)malloc(i * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
+	while (j < i)
+	{
+		s[j] = str[j];
+		j++;
+	}
+	s[j] = '\0';
+	return (s);
+}
+
